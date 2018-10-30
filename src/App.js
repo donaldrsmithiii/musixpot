1ebd31bb9cbf7919925a115f4a9707ed3397a900
Donald Smith III <donaldsmithiii@Donalds-MacBook-Pro.local>
2018-10-30 16:49:16 +0800

    Added Scroll Animation Feature to Side Drawer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './styles.css';
+import { Element } from 'react-scroll';
 
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
@@ -12,12 +13,28 @@ class App extends Component {
   render() {
     return (
       <div className="App" style={{height:'1500px'}}>
-        <Header />
-        <Featured />
-        <MusixInfo />
-        <Highlight />
-        <Pricing />
-        <Location />
+        <Header />  
+        
+        <Element name='featured'>
+          <Featured />
+        </Element>
+
+        <Element name='musixinfo'>
+          <MusixInfo />
+        </Element>
+        
+        <Element name='highlights'>
+          <Highlight /> 
+        </Element>
+        
+        <Element name='pricing'>
+          <Pricing />
+        </Element>
+        
+        <Element name='location'>
+          <Location />
+        </Element>
+
         <Footer />
       </div>
     );
--- src/components/header_footer/SideDrawer.js
+++ src/components/header_footer/SideDrawer.js
@@ -3,8 +3,20 @@ import React from 'react'
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import { scroller } from 'react-scroll';
 
 const SideDrawer = (props) => {
+
+    const scrollToSection = (section) => {
+      scroller.scrollTo(section,{
+        duration: 1500,
+        delay: 100,
+        smooth: true,
+        offset: -140,
+      });
+      props.onClose(false)
+    }
+
   return (
     <Drawer
       anchor='right'
@@ -12,19 +24,19 @@ const SideDrawer = (props) => {
       onClose={() => props.onClose(false)}
     >
       <List component='nav'>
-        <ListItem button onClick={() => console.log('featured')}>
+        <ListItem button onClick={() => scrollToSection('featured')}>
           Event Starts In
         </ListItem>
-        <ListItem button onClick={() => console.log('featured')}>
+        <ListItem button onClick={() => scrollToSection('musixinfo')}>
           Event Info
         </ListItem>
-        <ListItem button onClick={() => console.log('featured')}>
+        <ListItem button onClick={() => scrollToSection('highlights')}>
           Highlights
         </ListItem>
-        <ListItem button onClick={() => console.log('featured')}>
+        <ListItem button onClick={() => scrollToSection('pricing')}>
           Pricing
         </ListItem>
-        <ListItem button onClick={() => console.log('featured')}>
+        <ListItem button onClick={() => scrollToSection('location')}>
           Location
         </ListItem>
       </List>
